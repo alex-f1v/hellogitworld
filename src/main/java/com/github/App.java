33fba35312e2c7196faed4c427ae6331b4875715
ABraun <abraun@cainc.com>
2021-06-15 22:24:16 -0400

    feat(App): Add printing to text

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -5,11 +5,12 @@ package com.github;
  * Hello world!
  * Hello
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
     	//Comment
+        System.out.println( "To the SecondNewBranch!" );
         System.out.println( "Hello World!" );
     }
 }
